926d440d4d658e6327af03d552b90a7cc3206cd5
unknown <santiago.fernandez@grantitan.com>
2021-06-24 10:03:18 -0500

    Primer Commit Prueba

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Calendar } from "@ionic-native/calendar/ngx";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(private readonly calendar: Calendar) {}
+
+  calendario() {
+    this.calendar.createCalendar("MyCalendar").then(
+      (msg) => {
+        console.log(msg);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+}
